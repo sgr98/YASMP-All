dc33f5bfda301ddb2b52b7d3f772f7c35a243647
sgr98 <60283755+sgr98@users.noreply.github.com>
2023-04-15 19:00:55 +0530

    Added functionality for Causal Ordering

--- yasmpseer/routes/ordering.js
+++ yasmpseer/routes/ordering.js
@@ -6,16 +6,25 @@ const {
 const { getUserIndex } = require('./../utils.js');
 
 const conditionFIFO = (ind, vClockUser, vClockMessage) => {
-    return vClockUser[ind] + 1 === vClockMessage[ind]
-}
+    return vClockUser[ind] + 1 === vClockMessage[ind];
+};
 
 const conditionCausal = (ind, vClockUser, vClockMessage) => {
-    
-}
+    const cond1 = vClockMessage[ind] === vClockUser[ind] + 1;
+    const nUsers = vClockUser.length;
+    let cond2 = true;
+    for (let i = 0; i < nUsers; i++) {
+        if (i !== ind) {
+            if (!(vClockMessage[i] <= vClockUser[i])) {
+                cond2 = false;
+                break;
+            }
+        }
+    }
+    return cond1 && cond2;
+};
 
-const conditionTotal = (ind, vClockUser, vClockMessage) => {
-    
-}
+const conditionTotal = (ind, vClockUser, vClockMessage) => {};
 
 // Any order (User.Conversations)
 // {
@@ -42,7 +51,7 @@ const resolveFIFO = (conversations, message) => {
     // Check whether current message is ok
     if (conditionFIFO(fromInd, vClockUser, vClockMessage)) {
         conversations.data.push(message);
-        conversations.vectorClock[fromInd] += 1
+        conversations.vectorClock[fromInd] += 1;
     } else conversations.buffer.push(message);
 
     // Check whether any buffered messages are eligble to be added to data
@@ -52,7 +61,7 @@ const resolveFIFO = (conversations, message) => {
         vClockMessage = val.vectorClock[ORDERING_FIFO];
         if (conditionFIFO(fromInd, vClockUser, vClockMessage)) {
             conversations.data.push(val);
-            conversations.vectorClock[fromInd] += 1
+            conversations.vectorClock[fromInd] += 1;
         } else {
             newBufferConvos.push(val);
         }
@@ -63,7 +72,33 @@ const resolveFIFO = (conversations, message) => {
 };
 
 const resolveCausal = (conversations, message) => {
-    conversations.data.push(message);
+    // Message decoding
+    let fromInd = getUserIndex(message.from) - 1;
+    let vClockMessage = message.vectorClock[ORDERING_CAUSAL];
+
+    // Existing Convos
+    const vClockUser = conversations.vectorClock;
+
+    // Check whether current message is ok
+    if (conditionCausal(fromInd, vClockUser, vClockMessage)) {
+        conversations.data.push(message);
+        conversations.vectorClock[fromInd] = vClockMessage[fromInd];
+    } else conversations.buffer.push(message);
+
+    // Check whether any buffered messages are eligble to be added to data
+    let newBufferConvos = [];
+    conversations.buffer.map((val, ind, arr) => {
+        fromInd = getUserIndex(val.from) - 1;
+        vClockMessage = val.vectorClock[ORDERING_CAUSAL];
+        if (conditionCausal(fromInd, vClockUser, vClockMessage)) {
+            conversations.data.push(val);
+            conversations.vectorClock[fromInd] = vClockMessage[fromInd];
+        } else {
+            newBufferConvos.push(val);
+        }
+    });
+    conversations.buffer = newBufferConvos;
+
     return conversations;
 };
 
